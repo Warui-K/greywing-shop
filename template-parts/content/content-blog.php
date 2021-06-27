2fcb20f43685399961252d1b8788853f7a43cb9f
Warui-K <warui.kamiri@gmail.com>
2021-06-27 07:52:44 +0300

    Copying and Editing Greywing WP files

---
+++ template-parts/content/content-blog.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ */
+?>
+
+<div class="site-section bg-image overlay"
+    style="background-image: url('<?php echo get_theme_mod('gr_service_bg_about');?>');">
+    <div class="container">
+        <div class="row justify-content-center text-center">
+        <h1 class="mb-0"><?php the_title();?></h1>
+
+        </div>
+    </div>
+</div>
+
+
+<div class="site-section latest">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+
+
+                <article class="blog-post text-dark">
+                    <h2 class="blog-post-title"> <?php the_title();?></h2>
+                    <p class="blog-post-meta">Posted on <?php echo get_the_date();?> by <a href="<?php $author_URL;?>"><?php the_author();?></a></p>
+
+                    <p><?php the_content();?></p>
+                    <hr>
+                 </article>
+
+
+                <nav class="blog-pagination" aria-label="Pagination">
+                    <?php previous_post_link('%link');?>
+                   <?php next_post_link('%link'); ?>
+                </nav>
+
+            </div>
+
+
+        </div><!-- /.row -->
+    </div>
+</div>
